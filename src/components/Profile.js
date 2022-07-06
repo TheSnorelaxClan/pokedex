a6b851983a93494cd3080df98afab993efe4e751
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-07-06 12:39:08 -0700

    Pushing auth0 changes.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,7 @@ const Profile = () => {
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
+        <h3>{user.name}</h3>
         <p>{user.email}</p>
       </div>
     )
