d5b5f1f8f7fb2bb487abc1b38cca4179fe7ea49c
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-06 18:24:04 -0400

    Removed unneeded default code.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,8 +16,3 @@ root.render(
     <App />
   </Auth0Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
