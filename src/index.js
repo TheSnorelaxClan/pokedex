3bb7d4749f849948353a3505007158102df884b7
Matthew Larkin <matthewlarkin8@gmail.com>
2022-07-06 23:03:19 -0400

    Added routes to pages.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import Roster from './components/Roster'
+import AboutUs from './components/AboutUs'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
   <Auth0Provider
   domain={process.env.REACT_APP_AUTH_DOMAIN}
   clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
   redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI_REACT_SITE_URL}
   >
-    <App />
+    <Router>
+    <Header />
+    <Routes>
+      <Route
+        path="/"
+        element={<App/>}
+      />
+      <Route
+        path="/roster"
+        element={<Roster/>}
+      />
+      <Route
+        path="/about"
+        element={<AboutUs/>}
+      />
+    </Routes>          
+    <Footer />
+    </Router>
   </Auth0Provider>
+  </>
 );
