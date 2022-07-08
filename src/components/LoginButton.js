aa7531e0421e2db1bb89fe6d09a42e1876c8e69d
Stephanie Hill <stephni.hill@gmail.com>
2022-07-07 19:42:48 -0700

    login and out button moved

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,11 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import { Container } from "react-bootstrap";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <Button variant="outline-dark" onClick={() => loginWithRedirect()}>Log In</Button>;
+  return (
+    <>
+    <Container className="fixed-top mt-2">
+  <Button 
+  variant="outline-light" 
+  onClick={() => loginWithRedirect()}>Log In</Button>
+</Container>
+</>
+)
 };
 
 export default LoginButton;
